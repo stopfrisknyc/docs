81e628aa2ea3ef88b747960700136591a79d6555
Chris Herwig <chris@mapbox.com>
2012-10-07 14:55:47 -0400

    update js

---
+++ js/app.js
@@ -0,0 +1,6 @@
+
+$(document).ready(function() {
+	$('form').wrap("<div class='span12'>");
+	$('.table').wrap("<div class='span12'>");
+	$('.span-12').removeClass('span-12').addClass('span12');
+});
